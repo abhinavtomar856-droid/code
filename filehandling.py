da048eb9435fa05b624d2f86a2a887be33c2e18b
abhinavtomar856-driod <abhinavtomar856@gmail.com>
2025-11-24 18:34:27 +0530

    update files

---
+++ filehandling.py
@@ -0,0 +1,20 @@
+#File input and output in python(Notes by apnacollege)
+#Python can be used to perform operations on a file.(read and write data)
+
+#Type of all files:
+#1 Text files: .txt,.docx,.log etc
+#2 binary files:.mp4,.jpg,.mov etc 
+#3 CSV(comma separted vaues) files:
+
+# open,read and close a file:
+
+# We have to open a file before reading and writing
+
+#Syntax:
+# f=open("Filename",mode)
+# open() is built in function to open a file
+
+#data = f.read()
+# read() is a built in function to read data from a file
+
+#f.close()
